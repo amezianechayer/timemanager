0b6db05824be7513cc6ab7ff7345823a2f3c3e7d
badra31 <badar31@gmail.com>
2024-10-22 14:35:05 +0200

    Remove .git dir

---
+++ mobile-appplication/app/(tabs)/(home)/index.tsx
@@ -0,0 +1,37 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Bienvenue à la page d'accueil !</Text>
+      <Link href="/(home)/about" style={styles.button}>
+        Aller à la page à propos
+      </Link>
+      <Link
+        href={{
+          pathname: '/details/[id]',
+          params: { id: 'bacon' },
+        }}>
+        View user details
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
