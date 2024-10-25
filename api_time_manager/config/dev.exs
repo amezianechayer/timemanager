d6fe0da7ba8ae9b5741c1c1e4bd95758c7951158
frenchua-msc <francois-paul1.dinong@epitech.eu>
2024-10-25 11:24:08 +0200

    Update dev.exs

--- api_time_manager/config/dev.exs
+++ api_time_manager/config/dev.exs
@@ -2,22 +2,22 @@ import Config
 
 # Configure your database
 config :api_time_manager, ApiTimeManager.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "api_time_manager_dev",
-  stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
-
-# POUR ADRIEN
   # username: "postgres",
   # password: "postgres",
   # hostname: "localhost",
-  # database: "timemanager_dev",
+  # database: "api_time_manager_dev",
   # stacktrace: true,
   # show_sensitive_data_on_connection_error: true,
   # pool_size: 10
+
+# POUR ADRIEN
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "timemanager_dev",
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
